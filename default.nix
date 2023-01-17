e09faee10a9b6964718f4054a2877159e3231f6f
lezhou8 <lezhou@protonmail.com>
2023-01-17 14:06:59 +1100

    more intuitive integration positioning system

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ let
     url = "https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.0.fasta.gz";
     sha256 = "sha256-RFLpo174+DF3GPdh09J6HHO50NS439oJYWyuxDrWNG4=";
   };
-  chr1 = compression.uncompress { } chr1gz;
+  chr1 = compression.uncompress {} chr1gz;
   sequencelist = [ { fasta = hbv; name = "LC500247.1"; startingposition = 100; length = 80; gap = 100; }
                    { fasta = chr1; name = "chr1"; startingposition = 100; length = 80; gap = 100; } ];
 
--- runatposition.nix
+++ runatposition.nix
@@ -9,7 +9,8 @@ with bionix;
 with builtins;
 
 let
-  sectionfastas = x: callBionix ./samtools-faidx-edit.nix { regions = [ "${x.name}:${toString (x.position - x.length + 1)}-${toString x.position}" ]; } x.fasta;
+  sectionfastas = x: 
+    callBionix ./samtools-faidx-edit.nix { regions = [ "${x.name}:${toString x.position}-${toString (x.position + x.length)}" ]; } x.fasta;
   listsectionedfastas = map sectionfastas inputs;
   partsequences = callBionix ./mapmodifications.nix { modification = callBionix ./modifyfile.nix {}; } listsectionedfastas;
   togethersequence = callBionix ./catfiles.nix {} partsequences;
