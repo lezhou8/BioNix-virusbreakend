a9a7f70e0d3ede74cd09ad7c0f9e627da8ac67f5
lezhou8 <111640608+lezhou8@users.noreply.github.com>
2023-01-20 11:58:21 +1100

    lower iterations for testing

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { bionix }:
 
 with bionix;
+with compression;
 
 let
   hbv = fetchFastA {
@@ -11,9 +12,9 @@ let
     url = "https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.0.fasta.gz";
     sha256 = "sha256-RFLpo174+DF3GPdh09J6HHO50NS439oJYWyuxDrWNG4=";
   };
-  chr1 = compression.uncompress {} chr1gz;
+  chr1 = uncompress {} chr1gz;
   sequencelist = [ { fasta = hbv; name = "LC500247.1"; startingposition = 100; length = 80; gap = 100; }
                    { fasta = chr1; name = "chr1"; startingposition = 100; length = 80; gap = 100; } ];
 
 in
-  callBionix ./runiterations.nix { iterations = 3; productname = "chr1"; } sequencelist
+  callBionix ./runiterations.nix { iterations = 1; productname = "chr1"; } sequencelist
