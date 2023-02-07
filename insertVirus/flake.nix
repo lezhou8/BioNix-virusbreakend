8d33afae4a0e17b2204f60092867f69d8ed83c48
lezhou8 <lezhou@protonmail.com>
2023-02-07 17:31:00 +1100

    splitting function to separate file

---
+++ insertVirus/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Inserts viral genome into host genome";
+
+  inputs = {
+    bionix.url = "github:papenfusslab/bionix";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    bionix,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system:
+        with bionix.lib
+        {
+          overlays = [
+            (self: super: {
+              insert = self.callBionix ./insertVirus.nix;
+            })
+          ];
+          nixpkgs = import nixpkgs {inherit system;};
+        }; {
+          defaultPackage = callBionix ./. {};
+          packages.small = callBionix ./. {small = true;};
+        }
+    );
+}
