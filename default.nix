9a58036dc27cfc756d531272fbe43e455f965e55
lezhou8 <lezhou@protonmail.com>
2023-02-07 14:21:17 +1100

    init commit insert-test

--- default.nix
+++ default.nix
@@ -15,9 +15,5 @@ let
   };
   chr1 = uncompress {} chr1gz;
   insertVirus = callBionix ./insertVirus/insertVirus.nix;
-  viruses = [ hbv ];
-  positions = [ 1000000 ];
-  depths = [ 5 ];
-  test = map (virus: map (position: let virusReference = insertVirus { fasta = chr1; inherit position; } virus; in map (depth: samtools.sort {} (bwa.mem { ref = ref.grch38.seq; } { input1 = (art.illumina { inherit depth; } virusReference).out; input2 = (art.illumina { inherit depth; } virusReference).pair; })) depths) positions) viruses;
 in
-  (elemAt (elemAt (elemAt test 0) 0) 0)
+  insertVirus { fasta = chr1; position = 1000000; } hbv
