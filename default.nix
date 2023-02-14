c4b79b21b98bd08f23ab4d31d40319863fa4d2d7
lezhou8 <lezhou@protonmail.com>
2023-02-14 13:45:25 +1100

    added relative virus position selection

--- default.nix
+++ default.nix
@@ -17,6 +17,8 @@ let
   viruses = [ hbv ];
   positions = [ 1000000 ];
   depths = [ 5 ];
-  test = map (virus: map (position: let virusReference = insert { fasta = chr1; inherit position; virusPosition = position * 4 / 1000000; } virus; in map (depth: samtools.sort {} (bwa.mem { ref = ref.grch38.seq; } { input1 = (art.illumina { inherit depth; } virusReference).out; input2 = (art.illumina { inherit depth; } virusReference).pair; })) depths) positions) viruses;
+  multiplier = 4 / 1000000;
+  relativeVirusPosition = position: position * multiplier;
+  test = map (virus: map (position: let virusReference = insert { fasta = chr1; inherit position; virusPosition = relativeVirusPosition position; } virus; in map (depth: samtools.sort {} (bwa.mem { ref = ref.grch38.seq; } { input1 = (art.illumina { inherit depth; } virusReference).out; input2 = (art.illumina { inherit depth; } virusReference).pair; })) depths) positions) viruses;
 in
   (elemAt (elemAt (elemAt test 0) 0) 0)
