e2a6fa7f4a38e94aad970258d25ac10ae8a067ea
lezhou8 <lezhou@protonmail.com>
2023-02-14 17:31:25 +1100

    optional specify definition support

--- insertVirus/insertVirus.nix
+++ insertVirus/insertVirus.nix
@@ -5,6 +5,8 @@
 , position ? 1000000
 , virusPosition ? 4
 , fasta ? bionix.ref.grch38.seq
+, virusDefinition ? null
+, fastaDefinition ? null
 }:
 
 with bionix;
@@ -16,8 +18,8 @@ input:
 let
   queryRegion = callBionix ./samtools-queryRegion.nix;
   justSequence = callBionix ./modifyfile.nix {};
-  fastaName = callBionix ./getFastaDef.nix {} fasta;
-  virusName = callBionix ./getFastaDef.nix {} input;
+  fastaName = if fastaDefinition != null then fastaDefinition else callBionix ./getFastaDef.nix {} fasta;
+  virusName = if virusDefinition != null then virusDefinition else callBionix ./getFastaDef.nix {} input;
   left = justSequence (queryRegion { regions = [ "${fastaName}:${toString (position - flank)}-${toString position}" ]; } fasta);
   middle = justSequence (queryRegion { regions = [ "${virusName}:${toString (virusPosition)}-${toString (virusPosition + virusLength)}" ]; } input);
   right = justSequence (queryRegion { regions = [ "${fastaName}:${toString position}-${toString (position + breakOffset + flank)}" ]; } fasta);
